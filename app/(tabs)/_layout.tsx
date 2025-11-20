cee80431a5cd8703434af1310b63d424f707e942
Mart556 <marthaamer123@gmail.com>
2025-11-20 12:37:33 +0200

    refactor: remove unused state and effect from TabLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Slot } from 'expo-router';
 import NavBar from 'components/NavBar';
@@ -6,27 +6,16 @@ import Header from 'components/Header';
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-
-
 export default function TabLayout() {
   const insets = useSafeAreaInsets();
-  const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 5000); // 5000 ms = 5 seconds
-
-    return () => clearTimeout(timer); // cleanup
-  }, []);
 
   return (
     <View style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom }}>
-        <>
-          <Header />
-          <Slot />
-          <NavBar />
-        </>
+      <>
+        <Header />
+        <Slot />
+        <NavBar />
+      </>
     </View>
   );
 }
